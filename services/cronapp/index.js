11d61bb783384e2ae4caa72c160699b30f6b6ffc
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2022-10-07 00:00:06 +0900

    implement storage and cron

--- services/cronapp/index.js
+++ services/cronapp/index.js
@@ -1,11 +1,40 @@
 import { CronJob } from 'cron';
+import knex from './database/index.js';
 import uptime from '../uptime/index.js';
 
+
+const getUsers = async () => {
+  const result = await knex('users')
+    .select('id', 'username');
+  return result;
+}
+
+const getUrls = async (user_id) => {
+  const result = await knex('urls')
+    .where({ user_id });
+  return result;
+}
+
+const updateUrlStatus = async ({ id, status }) => {
+  const result = await knex('urls')
+    .where({ id })
+    .update({ status });
+  return result;
+}
+
 (async () => {
-    const job = new CronJob('0 * * * * *', async () => {
-        const response = await uptime('https://google.com', 200);
+  const job = new CronJob('* * * * * *', async () => {
+    const users = await getUsers();
+    users.forEach(async (user) => {
+      const urls = await getUrls(user.id);
+      urls.forEach(async (url) => {
+        const response = await uptime(url.address, url.status);
         console.log(response);
+        const row = await updateUrlStatus(url);
+        console.log(row);
+      })
     });
-    
-    job.start();
+  });
+
+  job.start();
 })();
