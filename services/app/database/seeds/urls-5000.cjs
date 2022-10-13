2a466a4bb2a63ca316db8eca3d05a3e2005ca77d
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2022-10-14 06:01:46 +0900

    add seeds

---
+++ services/app/database/seeds/urls-5000.cjs
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function(knex) {
+  const  ITERATION = 2;
+  const  NUM =  2500;
+  // Deletes ALL existing entries
+  await knex('urls').del()
+
+  for (let i = 0; i < ITERATION; i++) {
+    let urls = [];
+    for (let j = 0; j < NUM; j++) {
+      urls.push({ user_id: 10358896, address: `http://${(i * NUM) + (j + 1)}.monita.dhanifudin.com`, status: 200, hacked: false });
+    }
+    await knex('urls').insert(urls);
+  }
+
+};
