57a31462c21242fc82de8ba5a1d6754e21589714
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2022-10-13 22:19:00 +0900

    implement deface detection

--- services/uptime/index.js
+++ services/uptime/index.js
@@ -1,14 +1,22 @@
 import got from 'got';
+import similarity from 'string-cosine-similarity';
 
-const uptime = async (url, previousStatusCode) => {
-    const {statusCode} = await got.get(url);
-    if (statusCode > previousStatusCode) {
-        return { uptime: false, statusCode, message: `${url} is down` };
+const uptime = async (url, data = {body: '', statusCode: 200}) => {
+  const { body, statusCode } = await got.get(url);
+  let response = {};
+  if (statusCode > data.statusCode) {
+    return { uptime: false, body, statusCode, message: `${url} is down` };
+  }
+  if (statusCode <= data.statusCode) {
+    data.score || (data.score = 0.94);
+    if (data.body && similarity(body, data.body) < data.score) {
+      return { uptime: true, defaced: true, body, statusCode, message: `${url} is defaced` };
     }
-    if (statusCode < previousStatusCode) {
-        return { uptime: true, statusCode, message: `${url} has been recovered` };
+    if (statusCode < data.statusCode) {
+      response = { ...response, message: `${url} has been recovered` };
     }
-    return { uptime: (statusCode === 200), statusCode };
+    return { uptime: true, body, statusCode, ...response };
+  }
 }
 
 export default uptime;
